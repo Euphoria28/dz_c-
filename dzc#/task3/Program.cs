1a9e2b94625191a9d5e4b975f09656cf94ac3547
Euphoria <polunine28@gmail.com>
2022-12-08 20:45:52 +1100

    task3

---
+++ dzc#/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3: Напишите программу, которая выводит массив из 8 элементов, заполненный случайными числами. Оформите заполнение массива и вывод в виде функции 
+int[] CreateArray(int length, int minRange, int maxRange)
+{
+    int[] numbers = new int[length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(minRange, maxRange + 1);
+    }
+    return numbers;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+   
+    System.Console.WriteLine();
+}
+
+int length = 8;
+int minValue = 1;
+int maxValue = 8;
+System.Console.WriteLine("Массив из 8 элементов, заполненный случайными числами > ");
+int[] arr = CreateArray(length, minValue, maxValue);
+PrintArray(arr);
